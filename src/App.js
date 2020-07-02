15d7ff78152cb204b902bd416178979fd86a7e21
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-02 02:38:24 -0700

    constructor class for input to render input box

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button } from './Components/Button';
+import { Input } from './Components/Input';
 // import { render } from '@testing-library/react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: ""
+    }
+  }
   render() {
     return (
     <div className="app">
       <div className="calc-wrapper">
+        <Input input={this.state.input}></Input>
         <div className="row">
           <Button>7</Button>
           <Button>8</Button>
