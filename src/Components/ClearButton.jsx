45d4faf2039c96b65520de21ff81aad8696eda7c
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-02 03:32:37 -0700

    Edits

--- src/Components/ClearButton.jsx
+++ src/Components/ClearButton.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import './ClearButton.css';
+import React from "react";
+import "./ClearButton.css";
 
-export const ClearButton = (props) => (
-<div className="clear-btn"> onClick={props.handleClear}>
+export const ClearButton = props => (
+  <div className="clear-btn" onClick={props.handleClear}>
     {props.children}
-</div>
+  </div>
 );
