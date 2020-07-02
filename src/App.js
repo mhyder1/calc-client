b0908093538d6b3ff7711c22cf83a495a6f338cc
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-02 01:50:16 -0700

    import button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from './Components/Button';
 // import { render } from '@testing-library/react';
 
 class App extends Component {
   render() {
     return (
     <div className="app">
-      <div className="calc-wrapper">testing</div>
+      <div className="calc-wrapper">
+        <div className="row">
+
+        </div>
+      </div>
     </div>
   );
  };
